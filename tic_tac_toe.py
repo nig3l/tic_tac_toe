a52bab760dd3b172d155c47d705523ac16d13274
nigel chimwene <nigelchimwene@nigels-MacBook-Air.local>
2023-05-20 13:58:32 +0300

    second initial commit

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,7 +2,14 @@ import tkinter as tk
 from tkinter import font
 
 class Board(tk.Tk):
-    
+    def __init__(self):
+        super().__init__()
+        self.title("Boaard game")
+        self._cells = {}
+
+    def _create_board_display(self):
+        display_frame = tk.Frame(master=self)
+
 
 
 
