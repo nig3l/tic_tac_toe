ea7ff6018002b48bb75cee50bc5ed924aa9a6492
nigel <nigelchimwene@gmail.com>
2023-05-22 12:31:33 +0300

    s

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -13,7 +13,7 @@ class Board(tk.Tk):
         display_frame = tk.Frame(master=self) # to indicate the game's main window will be the frames' parent
 
         display_frame.pack(fill=tk.X) # for responsivenness of the window
-        
+
         self.display = tk.Label(
             master = display_frame,
             text = "ready?",
@@ -21,6 +21,11 @@ class Board(tk.Tk):
         )
         self.display.pack()
 
-
+#grid cells
+    def _create_board_grid(self):
+        grid_frame = tk.Frame(master=self)
+        grid_frame.pack()
+        for row in range(3):
+            
 
 
