dca9f15af662527fc86c0bb169d265def7070c90
nigel <nigelchimwene@gmail.com>
2023-06-02 06:47:41 +0300

    hotfix

--- exc.py
+++ exc.py
@@ -35,28 +35,7 @@ print("Vehicle Name:", School_bus.name, "Speed:", School_bus.max_speed, "Mileage
 # Give the capacity argument of Bus.seating_capacity() a default value of 50.
 # Expected output: The seating capacity of a bus is 50 passengers
 
-# class Vehicle:
-#     def __init__(self,name,maxspeed,mileage):
-#         self.name = name
-#         self.maxspeed = maxspeed
-#         self.mileage = mileage
 
-    
-# def seating_capacity(self, capacity):
-#         return f"The seating capacity of a {self.name} is {capacity} passengers"
-
-# class Bus(Vehicle):
-#     #  def __init__(self, name, maxspeed, mileage,seating_capacity):
-#     #       self.seating_capacity = seating_capacity
-#     #       super().__init__(name, maxspeed, mileage,seating_capacity)
-#     #  pass
-
-#     def seating_capacity(self , capacity=50):
-#         #  self.capacity = capacity
-#          return super().seating_capacity(capacity = 50)
-
-# Isuzu = Bus("bus",180,120)
-# print(Isuzu.seating_capacity())
 class Vehicle:
     def __init__(self, name, max_speed, mileage):
         self.name = name
@@ -68,8 +47,8 @@ class Vehicle:
 
 class Bus(Vehicle):
     # assign default value to capacity
-    def seating_capacity(self, capacity=50):
-        return super().seating_capacity(capacity=50)
+    def seating_capacity(self,capacity = 50):
+        return super().seating_capacity(capacity)
 
 School_bus = Bus("School Volvo", 180, 12)
 print(School_bus.seating_capacity())
