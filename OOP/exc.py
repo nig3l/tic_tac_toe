c381dc8572f647c4c9a6163462e2e3daf3fb88ba
nigel <nigelchimwene@gmail.com>
2023-07-04 14:50:52 +0300

    -

--- OOP/exc.py
+++ OOP/exc.py
@@ -527,5 +527,23 @@ print("Perimeter of the circle:", perimeter)
 # Include attributes like name, country and date of birth. 
 # Implement a method to determine the person's age. 
 
+'''
+from datetime import date
+class person:
+    def __init__(self,name,country,dob):
+        self.name = name
+        self.country = country
+        self.dob= dob
+
+    def get_age(self):
+
+        today = date.today()
+        age = today.year - self.date_of_birth.year
+        if today < date(today.year, self.date_of_birth.month, self.date_of_birth.day):
+            age -= 1
+
+        return age
+        
+ '''       
 
 
