f729b64eb0adde4c42d9645c7171f24ef5f18230
nigel <nigelchimwene@gmail.com>
2023-07-05 11:33:08 +0300

    q2

--- OOP/exc.py
+++ OOP/exc.py
@@ -571,6 +571,65 @@ class Calc:
 
 '''
 
+# Write a Python program to create a class that represents a shape. 
+# Include methods to calculate its area and perimeter. 
+# Implement subclasses for different shapes like circle, triangle, and square.
+
+
+import math
+
+class Shape:
+
+    def calc_area():
+        pass
+    def calc_perimeter():
+        pass
+
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+
+    def calc_area(self,radius):
+
+        return math.pi* radius ** 2
+    
+    def calc_perimeter(self,radius):
+
+        return math.pi * (radius+radius)
+    
+class Triangle(Shape):
+    def __init__(self,base, height, side1, side2, side3):
+        self.base = base
+        self.height = height
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+
+    def calculate_area(self):
+        return 0.5 * self.base * self.height
+
+    def calculate_perimeter(self):
+        return self.side1 + self.side2 + self.side3
+
+
+class Rectangle(Shape):
+    
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def calculate_area(self):
+        return self.length * self.width
+
+    def calculate_perimeter(self):
+        return 2 * (self.length + self.width)
+        
+    
+
+
+    
+        
+    
    
     
 
