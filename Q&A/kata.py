68e21b7599f04d6ab3ae3762c26460feafd64d2b
nigel <nigelchimwene@gmail.com>
2023-07-09 00:58:41 +0300

    Q&A

--- Q&A/kata.py
+++ Q&A/kata.py
@@ -89,6 +89,36 @@ def smash(words):
     sent = " ".join(words)
     return sent
 
+# Given a random non-negative number, 
+# you have to return the digits of this number within an array in reverse order.
+
+def digitize(n):
+    # Convert the number to a string, reverse it, and convert each character back to an integer
+
+    digits = [int(d) for d in str(n)][::-1]
+    return digits
+
+# When provided with a number between 0-9, return it in words.
+# Input :: 1
+# Output :: "One".
+
+def switch_it_up(number):
+     # Dictionary mapping numbers to words
+    number_words = {
+        0: "Zero",
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine"
+    }
+    
+    # Return the word for the given number
+    return number_words[number]
 
 
 
