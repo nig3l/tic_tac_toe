93db19156e550516610ee6df4a606e4726f942e6
nigel <nigelchimwene@gmail.com>
2023-07-09 00:10:00 +0300

    Q&A

--- Q&A/kata.py
+++ Q&A/kata.py
@@ -52,6 +52,32 @@ def check(seq, elem):
 
 
 
+# All of the animals are having a feast! Each animal is bringing one dish. 
+# There is just one rule: the dish must start and end with the same letters as the animal's name. 
+# For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+# Write a function feast that takes the animal's name and dish as arguments and returns true or false 
+# to indicate whether the beast is allowed to bring the dish to the feast.
+
+# Assume that beast and dish are always lowercase strings, and that each has at least two letters. 
+# beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. 
+# They will not contain numerals.
+
+
+def feast(beast, dish):
+ # Extract the first and last letters of the beast and dish
+    beast_start = beast[0]
+    beast_end = beast[-1]
+    dish_start = dish[0]
+    dish_end = dish[-1]
+    
+    # Check if the first and last letters match
+    if beast_start == dish_start and beast_end == dish_end:
+        return True
+    else:
+        return False
+
+
     
 
 
