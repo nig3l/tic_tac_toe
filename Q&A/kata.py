881af94beb6e1c468a38698a156ee50deb4cc154
nigel <nigelchimwene@gmail.com>
2023-07-10 01:01:00 +0300

    Q&A

--- Q&A/kata.py
+++ Q&A/kata.py
@@ -152,4 +152,30 @@ def rm_space(string):
 # Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
 
 # You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+import math
+def litres(time):
+    return math.floor(time*0.5)
+
+# You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+# Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+# Function should return true if it is possible and false if not
+
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    max_distance = mpg * fuel_left
+    if max_distance >= distance_to_pump:
+        return True
+    else:
+        return False
+    
+
+# In this kata you will create a function that takes a list of non-negative integers and strings 
+# and returns a new list with the strings filtered out.
+
+def filter_list(l):
+    return [x for x in l if not isinstance(x, str)]
+
+
+    
 
