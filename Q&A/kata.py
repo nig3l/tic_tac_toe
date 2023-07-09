1d9fd260722e4e6c7dc7fcd3a19ff840c22cb959
nigel <nigelchimwene@gmail.com>
2023-07-10 01:40:01 +0300

    Q&A

--- Q&A/kata.py
+++ Q&A/kata.py
@@ -201,6 +201,22 @@ def xo(s):
             count_o += 1
     return count_x == count_o
 
+# alternatively
+
+def xo(s):
+    s = s.lower()
+    return s.count('x') == s.count('o')
     
+
+
+# Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+# If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+def friend(x):
+    return [name for name in x if len(name)==4]
+    #Code
+
+
     
 
