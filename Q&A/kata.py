c294e08cbb46716656ad126f592a65fff6bab7c2
nigel <nigelchimwene@gmail.com>
2023-07-11 01:13:36 +0300

    Q&A

--- Q&A/kata.py
+++ Q&A/kata.py
@@ -273,6 +273,18 @@ def string_to_array(s):
 def invert(lst):
     return [-num for num in lst]
 
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+def bool_to_word(boolean):
+    if boolean == True:
+        return "Yes"
+    else:
+        return "No"
+    
+
+     
+    
+
+
 
 
     
