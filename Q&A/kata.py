89afaa0429f411ce255e08b22071d099dc487888
nigel <nigelchimwene@gmail.com>
2023-07-11 01:05:51 +0300

    Q&A

--- Q&A/kata.py
+++ Q&A/kata.py
@@ -243,6 +243,38 @@ def better_than_average(class_points, your_points):
 
 def lovefunc( flower1, flower2 ):
     return flower1 % 2 != flower2 % 2
+
+'''**********''' # You might know some pretty large perfect squares. But what about the NEXT one?
+
+# Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter.
+# Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+# If the parameter is itself not a perfect square then -1 should be returned. 
+# You may assume the parameter is non-negative.
+
+import math
+
+def find_next_square(sq):
+    root = math.isqrt(sq)
+    if root * root == sq:
+        return (root + 1) ** 2
+    else:
+        return -1
+
+# Write a function to split a string and convert it into an array of words.
+
+def string_to_array(s):
+    return s.split(' ')
+
+# 
+# Given a set of numbers, return the additive inverse of each. 
+# Each positive becomes negatives, and the negatives become positives.
+
+def invert(lst):
+    return [-num for num in lst]
+
+
+
     
 
 
