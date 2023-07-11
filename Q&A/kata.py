56695fe1e7d0cf1d9cd6e61c5ffae94b9de2e6ba
nigel <nigelchimwene@gmail.com>
2023-07-12 01:05:34 +0300

    Q&A

--- Q&A/kata.py
+++ Q&A/kata.py
@@ -281,8 +281,19 @@ def bool_to_word(boolean):
         return "No"
     
 
-     
+# Your task is to make a function that can take any non-negative integer as an argument 
+# and return it with its digits in descending order. 
+# Essentially, rearrange the digits to create the highest possible number.  
+
+def descending_order(num):
+    return int(''.join(sorted(str(num), reverse=True)))
+
     
+
+   
+      
+
+      
 
 
 
