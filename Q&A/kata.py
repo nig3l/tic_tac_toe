992145aa6c2498191dd0252f813bc39e5be7512f
nigel <nigelchimwene@gmail.com>
2023-07-13 00:57:07 +0300

    Q&A

--- Q&A/kata.py
+++ Q&A/kata.py
@@ -326,6 +326,28 @@ def disemvowel(string_):
         return string_
     
 
+# We need a function that can transform a string into a number. 
+# What ways of achieving this do you know?
+    
+def string_to_number(s):
+    return int(s)
+
+# Given an array of integers, remove the smallest value. 
+# Do not mutate the original array/list.
+# If there are multiple elements with the same value, remove the one with a lower index. 
+# If you get an empty array/list, return an empty array/list.
+
+# Don't change the order of the elements that are left.
+
+def remove_smallest(numbers):
+    if len(numbers) == 0:
+        return numbers
+    else:
+        min_index = numbers.index(min(numbers))
+        return numbers[:min_index] + numbers[min_index+1:]
+
+
+   
 
     
         
