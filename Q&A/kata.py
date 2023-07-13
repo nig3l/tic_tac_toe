0b24d7b82a1e98fa3f5b818a30fd5b3b3c544288
nigel <nigelchimwene@gmail.com>
2023-07-13 23:56:53 +0300

    Q&A

--- Q&A/kata.py
+++ Q&A/kata.py
@@ -361,7 +361,15 @@ def past(h, m, s):
 
     return time
    
+# Take 2 strings s1 and s2 including only letters from a to z.
+# Return a new sorted string, the longest possible, 
+# containing distinct letters - each taken only once - coming from s1 or s2.
 
+def longest(a1, a2):
+    distinct_letters = sorted(set(a1 + a2))
+    return ''.join(distinct_letters)
+
+    
    
 
     
